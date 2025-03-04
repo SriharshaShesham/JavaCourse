59d5039494e9626cfabc8a46c87e1adef9a5f3a1
Sriharsha Shesham <sriharsha@sheshams.in>
2025-03-03 19:48:00 -0500

    Introduction

---
+++ 0. Intro/Ramayanam.java
@@ -0,0 +1,20 @@
+// Libraries
+// import Ramayanam;
+
+//Class define
+public class Ramayanam {
+	// Naradudu -> Valmiki
+	public static void main(String[] args) {
+		System.out.println("Naradudu -> Valmiki");
+		// Balakanda();
+		Ayodyakanda();
+	}
+
+	public static void Balakanda() {
+		System.out.println("This is the first Kandam of the Ramayanam");
+	}
+
+	public static void Ayodyakanda() {
+		System.out.println("This is the second Kandam of the Ramayanam");
+	}
+}
