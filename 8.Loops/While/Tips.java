9f3223e620f082411ba2a6f253d3d236998a064d
Sriharsha Shesham <sriharsha@sheshams.in>
2025-04-01 20:47:26 -0400

    stuck with while

---
+++ 8.Loops/While/Tips.java
@@ -0,0 +1,14 @@
+public class Tips {
+	public static void main(String[] args) {
+		int theNumber = 0;
+		int limit = 10;
+		while (theNumber < limit) // 10 <= 10
+		{
+			theNumber++; // theNumber= 1+1 =2
+			// theNumber=theNumber+1;
+			System.out.println(theNumber);
+
+		}
+
+	}
+}
